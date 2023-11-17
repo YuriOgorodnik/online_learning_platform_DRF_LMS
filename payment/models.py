3567d609f877c2ae3d40329988a9d85ee80cbccc
User <yuri.ogorodnik@mail.ru>
2023-11-17 20:44:44 +0300

    update amount for course

--- payment/models.py
+++ payment/models.py
@@ -3,28 +3,42 @@ from users.models import User
 
 
 class Payment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="пользователь")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="пользователь"
+    )
     date = models.DateField(verbose_name="дата оплаты")
-    course = models.ForeignKey('course.Course', on_delete=models.CASCADE, null=True, blank=True,
-                               verbose_name="оплаченный курс")
-    lesson = models.ForeignKey('course.Lesson', on_delete=models.CASCADE, null=True, blank=True,
-                               verbose_name="оплаченный урок")
-    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="сумма оплаты")
+    course = models.ForeignKey(
+        "course.Course",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="оплаченный курс",
+    )
+    lesson = models.ForeignKey(
+        "course.Lesson",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="оплаченный урок",
+    )
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="сумма оплаты"
+    )
 
     PAYMENT_METHOD_CHOICES = [
-        ('cash', 'наличные'),
-        ('transfer', 'перевод на счет'),
+        ("cash", "наличные"),
+        ("transfer", "перевод на счет"),
     ]
     payment_method = models.CharField(
         max_length=20,
         choices=PAYMENT_METHOD_CHOICES,
-        default='cash',
-        verbose_name="способ оплаты"
+        default="cash",
+        verbose_name="способ оплаты",
     )
 
     def __str__(self):
-        return f'{self.user} - {self.date} - {self.amount}'
+        return f"{self.user} - {self.date} - {self.amount}"
 
     class Meta:
-        verbose_name = 'платёж'
-        verbose_name_plural = 'платежи'
+        verbose_name = "платёж"
+        verbose_name_plural = "платежи"
