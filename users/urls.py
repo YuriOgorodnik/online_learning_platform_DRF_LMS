40683f1c117d3bf7d53f4fa454d86df5ac4085fb
User <yuri.ogorodnik@mail.ru>
2023-11-17 20:46:47 +0300

    add login and authtoken

--- users/urls.py
+++ users/urls.py
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.views import (
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("user/update/<int:pk>/", UserUpdateAPIView.as_view(), name="user_update"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
