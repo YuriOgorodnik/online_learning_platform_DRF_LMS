53a3c264958f639b9f81d1378afc6783faae5c19
User <yuri.ogorodnik@mail.ru>
2023-11-17 20:48:34 +0300

    add user role

--- users/migrations/0003_user_role.py
+++ users/migrations/0003_user_role.py
@@ -4,15 +4,18 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('users', '0002_alter_user_managers'),
+        ("users", "0002_alter_user_managers"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='role',
-            field=models.CharField(choices=[('member', 'member'), ('moderator', 'moderator')], default='member', max_length=9),
+            model_name="user",
+            name="role",
+            field=models.CharField(
+                choices=[("member", "member"), ("moderator", "moderator")],
+                default="member",
+                max_length=9,
+            ),
         ),
     ]
