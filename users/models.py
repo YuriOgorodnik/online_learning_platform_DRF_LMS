654c886d54389216a423de7407f189dba95b7da8
User <yuri.ogorodnik@mail.ru>
2023-11-17 20:46:25 +0300

    add app users

--- users/models.py
+++ users/models.py
@@ -4,14 +4,14 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UserRoles(models.TextChoices):
-    MEMBER = 'member', _('member')
-    MODERATOR = 'moderator', _('moderator')
+    MEMBER = "member", _("member")
+    MODERATOR = "moderator", _("moderator")
 
 
 class UserManager(BaseUserManager):
     def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -19,27 +19,33 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
         return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name='электронная почта')
-    phone = models.CharField(max_length=35, verbose_name='номер телефона', null=True, blank=True)
-    city = models.CharField(max_length=50, verbose_name='город', null=True, blank=True)
-    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', null=True, blank=True)
-    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER)
+    email = models.EmailField(unique=True, verbose_name="электронная почта")
+    phone = models.CharField(
+        max_length=35, verbose_name="номер телефона", null=True, blank=True
+    )
+    city = models.CharField(max_length=50, verbose_name="город", null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to="users/", verbose_name="аватар", null=True, blank=True
+    )
+    role = models.CharField(
+        max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
