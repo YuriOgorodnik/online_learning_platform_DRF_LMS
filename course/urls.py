593e090476f7495413f4ff5169cd953580ae7d23
User <yuri.ogorodnik@mail.ru>
2023-11-17 20:50:32 +0300

    update file

--- course/urls.py
+++ course/urls.py
@@ -3,22 +3,40 @@ from django.urls import path
 from course.apps import CourseConfig
 from rest_framework.routers import DefaultRouter
 
-from course.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView, SubscriptionCourseCreateAPIView, SubscriptionCourseDestroyAPIView
+from course.views import (
+    CourseViewSet,
+    LessonCreateAPIView,
+    LessonListAPIView,
+    LessonRetrieveAPIView,
+    LessonUpdateAPIView,
+    LessonDestroyAPIView,
+    SubscriptionCourseCreateAPIView,
+    SubscriptionCourseDestroyAPIView,
+)
 
 app_name = CourseConfig.name
 
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet, basename='courses')
+router.register(r"courses", CourseViewSet, basename="courses")
 
 urlpatterns = [
-    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
-    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
-    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
-    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
-    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
-    path('subscription/create/<int:course_id>/', SubscriptionCourseCreateAPIView.as_view(), name='subscription_create'),
-    path('subscription/delete/<int:course_id>/', SubscriptionCourseDestroyAPIView.as_view(), name='subscription_delete'),
-
-
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
+    path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
+    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_get"),
+    path(
+        "lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson_update"
+    ),
+    path(
+        "lesson/delete/<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson_delete"
+    ),
+    path(
+        "subscription/create/<int:course_id>/",
+        SubscriptionCourseCreateAPIView.as_view(),
+        name="subscription_create",
+    ),
+    path(
+        "subscription/delete/<int:course_id>/",
+        SubscriptionCourseDestroyAPIView.as_view(),
+        name="subscription_delete",
+    ),
 ] + router.urls
